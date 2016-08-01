ca6ef3a69569aedf99d1d861b47963e1981ef25f
zhangzheng <18601254750.163.com>
2016-08-01 12:13:23 +0800

    增加meta.lua

---
+++ lua/meta.lua
@@ -0,0 +1,17 @@
+local version = setmetatable({
+  major = 0,
+  minor = 1,
+  patch = 0,
+  pre_release = "alpha"
+}, {
+  __tostring = function(t)
+    return string.format("%d.%d.%d%s", t.major, t.minor, t.patch,
+                         t.pre_release and "-"..t.pre_release or "")
+  end
+})
+
+return {
+  _NAME = "httptables",
+  _VERSION = tostring(version),
+  __VERSION = version
+}
