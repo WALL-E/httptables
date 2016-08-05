465716e897fe927603e0fdf8f8bf857166c38407
zhangzheng <zhangzheng@qianbao.com>
2016-08-05 08:45:57 +0000

    增加本地IP的测试用例

--- ci/spec/3_spec.js
+++ ci/spec/3_spec.js
@@ -1,5 +1,18 @@
 var frisby = require('frisby');
 
+function getIPAdress(){  
+    var interfaces = require('os').networkInterfaces();  
+    for(var devName in interfaces){  
+          var iface = interfaces[devName];  
+          for(var i=0;i<iface.length;i++){  
+               var alias = iface[i];  
+               if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){  
+                     return alias.address;  
+               }  
+          }  
+    }  
+}  
+
 frisby.create('origin')
   .get('http://127.0.0.1:8000/test/origin')
   .addHeader('X-Device-ID', 'device_1')
@@ -9,6 +22,18 @@ frisby.create('origin')
         status: 4001,
   })
 .toss();
+
+
+frisby.create('origin')
+  .get('http://' + getIPAdress() + ':8000/test/origin')
+  .addHeader('X-Device-ID', 'device_1')
+  .addHeader('X-User-ID', 'user_1')
+  .expectStatus(200)
+  .expectJSON({
+        status: 200,
+  })
+.toss();
+
 
 frisby.create('origin')
   .post('http://127.0.0.1:8000/test/origin')
@@ -29,4 +54,5 @@ frisby.create('origin')
         status: 200,
   })
 .toss();
+
 
