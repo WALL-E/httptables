19e4ee75c7eb649d38bcaccc6a6d2b7d9f55b7b8
zhangzheng <zhangzheng@qianbao.com>
2016-08-07 06:36:52 +0800

    增加policy.lua

--- lua/httptables.lua
+++ lua/httptables.lua
@@ -3,14 +3,30 @@
 local _M = {}
 
 local ngx = require "ngx"
+local cjson = require "cjson"
+local policy = require "policy"
+
+share_role_types = nil
+share_roles = nil
 
 
 function _M.init()
     ngx.log(ngx.INFO, "[Global] init")
+    
+    local data = ngx.shared.data
+    data:set("role_types_json", policy.role_types_json)
+    data:set("roles_json", policy.roles_json)
 end
 
 function _M.init_worker()
     ngx.log(ngx.INFO, "[Global] init_worker")
+
+    local data = ngx.shared.data
+    local role_types_json = data:get("role_types_json")
+    local roles_json = data:get("roles_json")
+
+    ngx.log(ngx.INFO, "role_types_josn: ", role_types_json)
+    ngx.log(ngx.INFO, "roles_json: ", roles_json)
 end
 
 return _M
---
+++ lua/policy.lua
@@ -0,0 +1,58 @@
+
+local _M = {}
+
+_M.role_types_json = [==[
+[
+	{
+		"enable": 1,
+		"name":"origin",
+		"priority":3
+	},
+	{
+		"enable":1,
+		"name":"device",
+		"priority":2},
+	{
+		"enable":1,
+		"name":"user",
+		"priority":1
+	}
+]
+]==]
+
+_M.roles_json = [==[
+[
+	{
+	    "response":"{\"status\":4001, \"message\":\"illegal origin\"}",
+	    "method":"get,post",
+	    "uri":"\/test\/origin",
+	    "judge":"reject",
+	    "type":"origin",
+	    "ttl":360000,
+	    "createtime":1470304637,
+	    "mark":"127.0.0.1"
+	},
+    {
+    	"response":"{\"status\":4003, \"message\":\"illegal device\"}",
+    	"method":"post",
+    	"uri":"\/test\/device",
+    	"judge":"reject",
+    	"type":"device",
+    	"ttl":360000,
+    	"createtime":1470304637,
+    	"mark":"device_1"
+    },
+    {
+    	"response":"{\"status\":4002, \"message\":\"illegal user\"}",
+    	"method":"get",
+    	"uri":"\/test\/user",
+    	"judge":"reject",
+    	"type":"user",
+    	"ttl":360000,
+    	"createtime":1470304637,
+    	"mark":"user_1"
+    }
+]
+]==]
+
+return _M
