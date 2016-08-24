a3ebc70c0d0077f1436452f6cc422289a7fbb807
zhangzheng <zhangzheng@qianbao.com>
2016-08-24 06:48:06 +0000

    重载js的格式化函数

--- ci/spec/config.js
+++ ci/spec/config.js
@@ -1,3 +1,26 @@
+String.prototype.replaceAll = function (exp, newStr) {
+    return this.replace(new RegExp(exp, "gm"), newStr);
+};
+
+String.prototype.format = function(args) {
+    var result = this;
+    if (arguments.length < 1) {
+        return result;
+    }
+
+    var data = arguments; 
+    if (arguments.length == 1 && typeof (args) == "object") {
+        data = args;
+    }
+    for ( var key in data) {
+        var value = data[key];
+        if (undefined != value) {
+            result = result.replaceAll("\\{" + key + "\\}", value);
+        }
+    }
+    return result;
+}
+
 config = {
     httptables: "127.0.0.1",
     httptables_manager: "127.0.0.1",
