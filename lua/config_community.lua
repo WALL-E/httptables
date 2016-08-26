60ba2598dcd50e74f9c0fd563535b1b9bce28b39
zhangzheng <zhangzheng@qianabo.com>
2016-08-26 06:46:51 +0000

    修改config_community.lua，httptables-manager默认端口为10085

--- lua/config_community.lua
+++ lua/config_community.lua
@@ -7,7 +7,7 @@ _M.redis.host = "127.0.0.1"
 _M.redis.port = "10379"
 
 _M.http_endpoint = {}
-_M.http_endpoint.role_types = "http://127.0.0.1:8080/apis/role_types/"
-_M.http_endpoint.roles = "http://127.0.0.1:8080/apis/roles/"
+_M.http_endpoint.role_types = "http://127.0.0.1:10085/apis/role_types/"
+_M.http_endpoint.roles = "http://127.0.0.1:10085/apis/roles/"
 
 return _M
